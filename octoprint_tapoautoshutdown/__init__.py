80f9377cf8a2164fa5f1f08cc8cfd2e657d0e7e1
SpideRaY <119012072+SpideRaY@users.noreply.github.com>
2026-08-22 13:11:43 +0100

    Add template configuration for Tapo Auto Shutdown

--- octoprint_tapoautoshutdown/__init__.py
+++ octoprint_tapoautoshutdown/__init__.py
@@ -59,6 +59,15 @@ class TapoAutoShutdownPlugin(
                 e,
             )
 
+    def get_template_configs(self):
+        return [
+            {
+                "type": "settings",
+                "name": "Tapo Auto Shutdown",
+                "template": "tapoautoshutdown_settings.jinja2",
+            }
+        ]
+
     def get_settings_defaults(self):
         return {
             "username": "",
